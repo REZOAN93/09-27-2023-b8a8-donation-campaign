e53037953b05ddeb63d745699317b7659960781c
REZOAN93 <rezoanshawonweb@gmail.com>
2023-09-26 00:29:49 +0600

    updated donation page

--- src/Components/Campaign/Details.jsx
+++ src/Components/Campaign/Details.jsx
@@ -3,23 +3,31 @@ import { useEffect } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const Details = () => {
-    const data=useLoaderData()
-    const [specificData,setSpecificData]=useState([])
-    const {id}=useParams()
-    const idInt=parseInt(id)
-    useEffect(()=>{
-       const SingleData=data.find((na)=>na.id===idInt)
-       setSpecificData(SingleData)
-    },[data,idInt])
-    
-    console.log(specificData)
+  const data = useLoaderData();
+  const { id } = useParams();
+  const idInt = parseInt(id);
+  const specificData = data.find((na) => na.id === idInt);
+  const {name,donation_amount,description,picture2}=specificData
+  console.log(specificData);
 
-    return (
-        <div className="max-w-7xl mx-auto border my-10">
-           
-
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto my-2 pb-20">
+      <div className="card space-y-5">
+        <figure className="w-full h-screen rounded-lg">
+          <img
+          className="w-full h-fit rounded-lg"
+            src={picture2}
+            alt="Shoes"
+          />
+        </figure>
+       
+          <h2 className=" text-4xl">
+            {name}
+          </h2>
+          <p className=" text-base text-justify text-gray-500">{description}</p>      
+      </div>
+    </div>
+  );
 };
 
 export default Details;
